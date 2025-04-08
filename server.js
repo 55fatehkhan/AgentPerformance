0e2d4425d4b3606d51949ea06cb7a2ffc626a36e
55fatehkhan <55fatehkhan44@gmail.com>
2025-04-08 22:33:15 +0530

    fixed fetch issue

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
-const fetch = require('node-fetch');
 
 const app = express();
 const port = process.env.PORT || 5000;
